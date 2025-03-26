e5e6d5e831c629245a3f97768efb9d1607caf787
Flaze07 <Flaze07@users.noreply.github.com>
2025-03-26 19:46:35 +0700

    added telescope-live-grep-args for better grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,30 @@
-return {
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
-        vimgrep_arguments = {
-          "rg",
-          "--color=never",
-          "--no-heading",
-          "--with-filename",
-          "--line-number",
-          "--column",
-          "--smart-case",
-          "--hidden",
-        },
+local M = {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-live-grep-args.nvim",
+    version = "^1.0.0",
+  },
+  opts = {
+    defaults = {
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--hidden",
       },
     },
   },
 }
+
+M.config = function()
+  -- local keyset = vim.keymap.set
+  -- local opts = { silent = true, noremap = false, expr = true, replace_keycodes = false }
+  -- keyset("n", "<leader>fg", ':lua require("telescope:).extensions.live_grep_args.live_grep_args()<CR>', opts)
+  vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+end
+
+return M
